b09e93caa17697cf84a65bfb2563e6de1710790b
Nilesh Raut <nileshraut898@gmail.com>
2026-02-09 11:17:28 +0530

    deployment

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -3,7 +3,7 @@ import rateLimit from 'express-rate-limit';
 // General API rate limiter
 export const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 10000, // Limit each IP to 100 requests per window
+    max: 100, // Limit each IP to 100 requests per window
     message: {
         message: 'Too many requests from this IP, please try again after 15 minutes',
     },
